46607c7138966512a163614f368f40bd9b675bff
LukaszCmielowski <Lukasz.Cmielowski@pl.ibm.com>
2016-12-07 13:33:54 +0100

    update

--- script/KafkaProducer.py
+++ script/KafkaProducer.py
@@ -5,6 +5,7 @@ import json
 
 ############################################
 # Service credentials from Bluemix UI:
+# !Please replace below examples with your credentials
 ############################################
 bootstrap_servers = "kafka01-prod01.messagehub.services.us-south.bluemix.net:9093"
 sasl_plain_username = "gkyIt44JC9S1eGDv"
@@ -28,18 +29,8 @@ producer = KafkaProducer(bootstrap_servers = bootstrap_servers,
                          api_version=(0,10),
                          value_serializer = lambda v: json.dumps(v).encode('utf-8'))
 
-# Asynchronous by default
-#future = producer.send('umitsInput', b'raw_bytes')
+# !Put your topic name and tweet message here
 future = producer.send('umitsInput', {'tweet': 'umits tweet is sooo cooooll'})
-
-
-# Block for 'synchronous' sends
-try:
-    record_metadata = future.get(timeout=10)
-except KafkaError:
-    # Decide what to do if produce request failed...
-    log.exception()
-    pass
 
 # Successful result returns assigned partition and offset
 print (record_metadata.topic)
